bff79971ac7c97cadf967859447cd53f845e18d8
Dan Blondell <danblondell@gmail.com>
2017-11-19 04:13:12 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -29,40 +29,33 @@ class PigLatinizer
   end
 
 
-
-
-
-
-
-
-
-  def make_piglatin_array
-    text_array = @text.split(" ")
-
-    text_array.map do | word |
-      word.downcase!
-      first_letter = word[0]
-      second_letter = word[1]
-
-      punctuation = word.scan(/[^a-z]/).first
-
-      punctuation ? (word_end = "ay" + punctuation) : (word_end = "ay")
-      word.slice!(punctuation) if punctuation
-
-      if !first_letter.scan(/[aeiou]/).empty?
-        word + "w" + word_end
-      elsif first_letter.scan(/[aeiou]/).empty? && second_letter.scan(/[aeiou]/).empty? && word.size > 1
-        word[2..-1] + first_letter + second_letter + word_end
-      else
-        word[1..-1] + first_letter + word_end
-      end
-
-    end
-  end
-
-  def piglatinize
-    make_piglatin_array.join(" ")
-  end
+  # def make_piglatin_array
+  #   text_array = @text.split(" ")
+  # 
+  #   text_array.map do | word |
+  #     word.downcase!
+  #     first_letter = word[0]
+  #     second_letter = word[1]
+  # 
+  #     punctuation = word.scan(/[^a-z]/).first
+  # 
+  #     punctuation ? (word_end = "ay" + punctuation) : (word_end = "ay")
+  #     word.slice!(punctuation) if punctuation
+  # 
+  #     if !first_letter.scan(/[aeiou]/).empty?
+  #       word + "w" + word_end
+  #     elsif first_letter.scan(/[aeiou]/).empty? && second_letter.scan(/[aeiou]/).empty? && word.size > 1
+  #       word[2..-1] + first_letter + second_letter + word_end
+  #     else
+  #       word[1..-1] + first_letter + word_end
+  #     end
+  # 
+  #   end
+  # end
+  # 
+  # def piglatinize
+  #   make_piglatin_array.join(" ")
+  # end
 
 
 end
