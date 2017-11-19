d6e058bf7ef5230f85e888bce36a90f2e539037f
Dan Blondell <danblondell@gmail.com>
2017-11-19 03:33:53 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -18,7 +18,7 @@ class PigLatinizer
       word.split!(punctuation)
 
       if !first_letter.scan(/[aeiou]/).empty?
-        word + "ay"
+        word + "ay" + punctuation
 
       elsif first_letter.scan(/[aeiou]/).empty? && second_letter.scan(/[aeiou]/).empty?
         new_ending = first_letter + second_letter + "ay"
