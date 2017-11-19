f07858c0a69ceec72c1a7ce9c7dea3f775b91f66
Dan Blondell <danblondell@gmail.com>
2017-11-19 04:12:21 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,22 @@ class PigLatinizer
   end
 
   def piglatinize
+    word.downcase!
+    first_letter = word[0]
+    second_letter = word[1]
 
+    punctuation = word.scan(/[^a-z]/).first
+
+    punctuation ? (word_end = "ay" + punctuation) : (word_end = "ay")
+    word.slice!(punctuation) if punctuation
+
+    if !first_letter.scan(/[aeiou]/).empty?
+      word + "w" + word_end
+    elsif first_letter.scan(/[aeiou]/).empty? && second_letter.scan(/[aeiou]/).empty? && word.size > 1
+      word[2..-1] + first_letter + second_letter + word_end
+    else
+      word[1..-1] + first_letter + word_end
+    end
   end
 
 
