33b8e0c6b47eab66d1f29f97293c0cb646226783
Dan Blondell <danblondell@gmail.com>
2017-11-19 03:23:31 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,9 +14,8 @@ class PigLatinizer
         word + "ay"
       elsif first_letter.scan(/[aeiou]/).empty? && second_letter.scan(/[aeiou]/).empty?
         new_ending = first_letter + second_letter + "ay"
-        word.slice!(0..1)
 
-        word + new_ending
+        word[2..-1] + new_ending
       else
         word.[1..-1] + first_letter + "ay"
       end
