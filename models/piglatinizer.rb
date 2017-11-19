319804db65a9725ee50a4b80b97fcd068418d635
Dan Blondell <danblondell@gmail.com>
2017-11-19 04:46:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
 
   def piglatinize(word)
-    first_letter = word[0]
+    first_letter = word[0] # would downcase here but text reqires not downcasing
     second_letter = word[1]
     third_letter = word[2]
 
