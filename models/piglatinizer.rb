f454412edb6e4f020ef02d93cd3dc7eccc5fcd3b
Dan Blondell <danblondell@gmail.com>
2017-11-19 03:09:14 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,10 +7,12 @@ class PigLatinizer
 
   def piglatinize
     @text.map do | word |
+      first_letter = word[0]
+      second_letter = word[1]
       if word[0] == /[aeiou]/
         word + "ay"
       elsif word[0] != /[aeiou]/ && word[1] != /[aeiou]/
-        word
+        first_letter = word[0]
     end
   end
 
