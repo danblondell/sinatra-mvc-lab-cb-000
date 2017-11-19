924e0814e0399702b92c922c2c210e7c5c1e4df7
Dan Blondell <danblondell@gmail.com>
2017-11-19 03:50:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -21,7 +21,7 @@ class PigLatinizer
       elsif first_letter.scan(/[aeiou]/).empty? && second_letter.scan(/[aeiou]/).empty?
         word[2..-1] + first_letter + second_letter + word_end
       else
-        word[1..-1] + first_letter + "ay" + punctuation
+        word[1..-1] + first_letter +  word_end
       end
 
     end
