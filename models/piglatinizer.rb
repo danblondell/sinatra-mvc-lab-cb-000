38a72c783e304b8c1da196cf01e43b0dfe50da97
Dan Blondell <danblondell@gmail.com>
2017-11-19 03:26:18 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,22 +6,10 @@ class PigLatinizer
   end
 
   def make_piglatin_array
+  end
 
   def piglatinize
-    @text.map do | word |
-      first_letter = word[0]
-      second_letter = word[1]
 
-      if !first_letter.scan(/[aeiou]/).empty?
-        word + "ay"
-      elsif first_letter.scan(/[aeiou]/).empty? && second_letter.scan(/[aeiou]/).empty?
-        new_ending = first_letter + second_letter + "ay"
-
-        word[2..-1] + new_ending
-      else
-        word[1..-1] + first_letter + "ay"
-      end
-    end
   end
 
 
