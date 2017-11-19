fc3ee7b9ff696bf076d7f03820c49a5d7f8d06f6
Dan Blondell <danblondell@gmail.com>
2017-11-19 03:05:58 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
     @text.map do | word |
       if word.first == /[aeiou]/
         word + "ay"
-      elsif word.first == /[]/
+      elsif word.first == /[^aeiou]/ && word[1] == /[^aeiou]/
 
     end
   end
